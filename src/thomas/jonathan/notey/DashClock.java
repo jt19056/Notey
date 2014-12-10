bb162ec62acc756e00fa0bfacb51e95fb4307f58
jt19056 <jthomas92@knights.ucf.edu>
2014-12-09 19:19:00 -0500

    v2.4.0.1
    Dashclock api, voice input, share to other apps, 'info screen'

---
+++ src/thomas/jonathan/notey/DashClock.java
@@ -0,0 +1,52 @@
+package thomas.jonathan.notey;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.google.android.apps.dashclock.api.DashClockExtension;
+import com.google.android.apps.dashclock.api.ExtensionData;
+
+import java.util.List;
+
+public class DashClock extends DashClockExtension {
+    private MySQLiteHelper db = new MySQLiteHelper(this);
+
+    protected void onUpdateData(int reason) {
+        List<NoteyNote> allNoteys = db.getAllNoteys();
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        String dashclock_icon;
+        int icon = R.drawable.ic_check_white_36dp; //default icon is white check
+
+        // get icon from preferences
+        dashclock_icon = sharedPref.getString("pref_dashclock_icon", "check");
+
+        // set icon
+        if(dashclock_icon.equals("check")) icon = R.drawable.ic_check_white_36dp;
+        else if(dashclock_icon.equals("warning")) icon = R.drawable.ic_warning_white_36dp;
+        else if(dashclock_icon.equals("edit")) icon = R.drawable.ic_edit_white_36dp;
+        else if(dashclock_icon.equals("star")) icon = R.drawable.ic_star_white_36dp;
+        else if(dashclock_icon.equals("whatshot")) icon = R.drawable.ic_whatshot_white_36dp;
+
+        if(allNoteys.size() > 0) {
+            String allNoteysString = "";
+            for (NoteyNote n : allNoteys) {
+                allNoteysString += n.getNote() + "\n";
+
+                if(dashclock_icon.equals("topIcon")) icon = n.getIcon();
+            }
+
+            publishUpdate(new ExtensionData()
+                    .visible(true)
+                    .icon(icon)
+                    .status(allNoteys.size() + "")
+                    .expandedTitle(allNoteys.size() + " " + getResources().getString(R.string.reminders))
+                    .expandedBody(allNoteysString)
+                    .clickIntent(new Intent(this, MainActivity.class)));
+        }
+        else publishUpdate(null);
+
+    }
+
+
+}
---
+++ src/thomas/jonathan/notey/DashClockSettingsActivity.java
@@ -0,0 +1,31 @@
+package thomas.jonathan.notey;
+
+import android.content.SharedPreferences;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.PreferenceActivity;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class DashClockSettingsActivity extends PreferenceActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.settings_dashclock);
+
+        //show action bar
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+        getActionBar().setIcon(new ColorDrawable(getResources().getColor(android.R.color.transparent)));
+    }
+
+    @Override //back button in action bar to close settings
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            this.finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
