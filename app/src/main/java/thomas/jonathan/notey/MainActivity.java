3bbec76ec1ded1a789a0cc710f009c41d971c329
jt19056 <jthomas92@knights.ucf.edu>
2016-03-12 17:07:59 -0500

    Case 82
    Fix for "Share to Notey.." Title would not populate.
    
    Closes #82

--- app/src/main/java/thomas/jonathan/notey/MainActivity.java
+++ app/src/main/java/thomas/jonathan/notey/MainActivity.java
@@ -6,7 +6,6 @@ import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
@@ -1086,6 +1085,8 @@ public class MainActivity extends Activity implements OnClickListener, View.OnLo
             et.setSelection(et.getText().length());
 
             noteTitle = intent.getStringExtra(Intent.EXTRA_SUBJECT);
+            et_title.setText((noteTitle));
+            et_title.setSelection(et_title.getText().length());
         }
     }
 
